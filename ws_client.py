5b458acfacdc4046bd76e84761f1f4819ae8586e
Steve <ideabus@users.noreply.github.com>
2025-07-28 17:49:16 +0900

    Update ws_client.py
    to_csv 기능 추가

--- ws_client.py
+++ ws_client.py
@@ -1,9 +1,33 @@
 import asyncio
 import json
 from pprint import pprint
-from xml.etree.ElementTree import indent
 import websockets
 from datetime import datetime
+import csv
+import os.path
+#
+def to_csv(data, filename='output.csv'):
+    # Open the CSV file in write mode with 'newline=' for proper CSV formatting
+    print("to_csv", filename)
+    #
+    if not os.path.isfile(filename):
+        with open(filename, mode='w+', newline='\n') as csvfile:
+            # Create a DictWriter object
+            fieldnames = data.keys() if isinstance(data, dict) else data[0].keys()
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+        #
+    # Append mode
+    with open(filename, mode='at', newline='\n') as csvfile:
+        # Create a DictWriter object
+        fieldnames = data.keys() if isinstance(data, dict) else data[0].keys()
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        # Write the data rows
+        writer.writerow(data)
+
+    print(f"Data successfully written to {filename}")
+    return True
 #
 async def subscribe(channel, target_currency="BTC", quote_currency="KRW"):
     """https://docs.coinone.co.kr/reference/public-websocket-1"""
@@ -37,6 +61,8 @@ async def subscribe(channel, target_currency="BTC", quote_currency="KRW"):
             if 'data' in response_json and 'last' in response_json['data']:
                 print(diff_secs, 'last:',json.dumps(response_json['data']['last'], indent=2, ensure_ascii=False))
                 print(json.dumps(response_json['data'], indent=2, ensure_ascii=False))
+                #to_csv(response_json['data'], filename=f"{channel}_{target_currency}_{quote_currency}.csv")
+                to_csv(response_json['data'])
             if response_json["response_type"] == "ERROR":
                 print("error_code", response_json["error_code"])
             if response_json["response_type"] == "PONG":
@@ -56,3 +82,4 @@ if __name__ == '__main__':
     asyncio.run(gather())
 #
 # 출처: https://comdoc.tistory.com/entry/파이썬-코인원-웹-소켓-데모 [ComDoc:티스토리]
+#
